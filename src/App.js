fe3e3d6992da7bc4a505737192cec53ea20c01d0
SarahTouaiti <touaiti.sara@gmail.com>
2025-06-11 17:11:54 +0200

    Add some basic HTML

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import "Weather.js";
+import Weather from "./Weather.js";
 
 function App() {
   return (
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,45 @@
 import React from "react";
 import "./Weather.css";
 export default function Weather() {
-  return <div className="Weather">Hello</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://www.clipartmax.com/png/middle/129-1293850_download-icon-mostly-cloudy-weather-icon.png"
+            alt="Mostly Cloudy"
+            width={50}
+            height={50}
+          />
+          6°C
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 13 Km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
