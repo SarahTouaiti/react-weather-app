dca19195c8a8110f6224f41cf316bfa78504e20e
SarahTouaiti <touaiti.sara@gmail.com>
2025-06-11 15:52:33 +0200

    added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./App.css";
+import "Weather.js";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/sara-touaiti-0350841b4/"
-          target="blank"
-        >
-          Sara Touaiti
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a href="https://github.com/SarahTouaiti" target="blank">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/sara-touaiti-0350841b4/"
+            target="blank"
+          >
+            Sara Touaiti
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a href="https://github.com/SarahTouaiti" target="blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
