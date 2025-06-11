2895f4f471ddeb96e2ea1da2fc5c64d1d917ad15
SarahTouaiti <touaiti.sara@gmail.com>
2025-06-11 14:55:22 +0200

    added a footer
    -my linkedIn profile
    -my GitHub profile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by
+        <a
+          href="https://www.linkedin.com/in/sara-touaiti-0350841b4/"
+          target="blank"
+        >
+          Sara Touaiti
+        </a>
+        and is open-sourced on{" "}
+        <a href="https://github.com/SarahTouaiti" target="blank">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
